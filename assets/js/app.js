50a871934820524d34711081864209f5d8a34fdf
Holanad <volkov99.at@gmail.com>
2026-09-24 23:57:51 +0300

    feat: add resize cards Adaptive js

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,6 +85,13 @@ const headerAdaptive = () => {
                 html.classList.remove('hidden');
             }
         })
+        window.addEventListener('resize', () => {
+            if(window.innerWidth >= 768) {
+                headerMobileElem.classList.remove('open');
+                headerBurgerButtonElem.classList.remove('open');
+                html.classList.remove('hidden');
+            }
+        })
     };
 
 
@@ -99,60 +106,122 @@ headerAdaptive();
 const coffieMenuSettings = () => {
 
     const tabsBtn = document.querySelectorAll('.tabs__button');
+    let currentCardsShow = 0;
+
+    const countLoadCards = {
+        992 : 8,
+        320 : 4,
+    }
+
+    let dataCoffee = [];
+
+    const sizeWindowShow = () => {
+        Object.keys(countLoadCards).forEach((elem, index) => {
+            if(window.innerWidth >= elem) {
+                currentCardsShow = Object.values(countLoadCards)[index];
+            }
+        })
+        return currentCardsShow;
+    }
+    sizeWindowShow();
 
     fetch('assets/json/products.json')
     .then(response => response.json())
     .then(jsonData => {
-        loadCoffee(tabsBtn[0].getAttribute('data-coffee'), jsonData);
-        switchCategoryCoffee(jsonData)
+        dataCoffee = jsonData;
+
+        loadCoffee();
+        switchCategoryCoffee();
     });
 
-    const loadCoffee = (categoryCoffee, dataCoffee) => {
+    const loadCoffee = (categoryCoffee = tabsBtn[0].getAttribute('data-coffee'), countLoad = currentCardsShow) => {
+        
         const listCoffee = document.querySelector('.tabs-block .cards');
-
         listCoffee.innerHTML = '';
         
         let resultCoffe = ``;
+        let countCardsInCategory = 0;
+        
         for(let i = 0; i < dataCoffee.length; i++) {
             if(categoryCoffee == dataCoffee[i].category) {
-                resultCoffe += `
-                    <li class="card" data-title="${dataCoffee[i].name}">
-                        <div class="card-image">
-                            <img src="${dataCoffee[i].image}" alt="Irish coffee" class="img-cover">
-                        </div>
-                        <div class="card-info f-dc">
-                            <h2 class="card__name">
-                                ${dataCoffee[i].name}
-                            </h2>
-                            <p class="card__descr">
-                                ${dataCoffee[i].description}
-                            </p>
-                            <p class="card__price">
-                                $${dataCoffee[i].price}
-                            </p>
-                        </div>
-                    </li>
-                `
+                countCardsInCategory++;
+                if(countCardsInCategory <= countLoad) {
+                    document.querySelector('.card-more').classList.add('none');
+                    resultCoffe += `
+                        <li class="card" data-title="${dataCoffee[i].name}">
+                            <div class="card-image">
+                                <img src="${dataCoffee[i].image}" alt="Irish coffee" class="img-cover">
+                            </div>
+                            <div class="card-info f-dc">
+                                <h2 class="card__name">
+                                    ${dataCoffee[i].name}
+                                </h2>
+                                <p class="card__descr">
+                                    ${dataCoffee[i].description}
+                                </p>
+                                <p class="card__price">
+                                    $${dataCoffee[i].price}
+                                </p>
+                            </div>
+                        </li>
+                    `;
+                } else {
+                    document.querySelector('.card-more').classList.remove('none');
+                }
             }
         }
         listCoffee.innerHTML = resultCoffe;
         popupSettings(dataCoffee);
     }
 
-
-    const switchCategoryCoffee = (dataCoffee) => {
+    const switchCategoryCoffee = () => {
         tabsBtn.forEach((e) => {
             e.addEventListener('click', () => {
                 tabsBtn.forEach((s) => {
                     s.classList.remove('active')
                 })
                 e.classList.add('active')
-                loadCoffee(e.getAttribute('data-coffee'), dataCoffee)
+
+                Object.keys(countLoadCards).forEach((elem, index) => {
+                    if(window.innerWidth >= elem) {
+                        cardsItemShow = Object.values(countLoadCards)[index]
+                    }
+                })
+
+                currentCardsShow = sizeWindowShow();
+
+                loadCoffee(e.getAttribute('data-coffee'))
             })
         })
     }
+
+    const loadMoreCards = () => {
+        document.querySelector('.card-more').addEventListener('click', () => {
+            let categoryBlock;
+            tabsBtn.forEach((e) => {
+                if(e.classList.contains('active')) {
+                    categoryBlock = e;
+                }
+            })
+            currentCardsShow += sizeWindowShow();
+
+            loadCoffee(categoryBlock.getAttribute('data-coffee'), currentCardsShow)
+        })
+        
+        window.addEventListener('resize', () => {
+            let categoryBlock;
+            tabsBtn.forEach((e) => {
+                if(e.classList.contains('active')) {
+                    categoryBlock = e;
+                }
+            })
+            currentCardsShow = sizeWindowShow();
+            loadCoffee(categoryBlock.getAttribute('data-coffee'), currentCardsShow)
+        })
+    }
+    loadMoreCards();
     
-    const popupSettings = (dataCoffee) => {
+    const popupSettings = () => {
 
         const cardElems = document.querySelectorAll('.card')
         const modalCoffeeElem = document.querySelector('.popup-coffee');
@@ -161,13 +230,17 @@ const coffieMenuSettings = () => {
             cardElems.forEach((e) => {
                 e.addEventListener('click', () => {
                     const cardCurrent = e.getAttribute('data-title');
+                    let totalCardPrice = 0;
+
                     for(let i = 0; i < dataCoffee.length; i++) {
                         if(cardCurrent == dataCoffee[i].name) {
 
                             modalCoffeeElem.querySelector('.popup-coffee-image img').setAttribute('src', dataCoffee[i].image);
                             modalCoffeeElem.querySelector('.popup-coffee__title').textContent = dataCoffee[i].name;
                             modalCoffeeElem.querySelector('.popup-coffee__descr').textContent = dataCoffee[i].description;
-                            modalCoffeeElem.querySelector('.popup-coffee-total__name').textContent = '$' + dataCoffee[i].price;
+                            modalCoffeeElem.querySelector('.popup-coffee-total__price').textContent = '$' + dataCoffee[i].price;
+                            modalCoffeeElem.querySelector('.popup-coffee-total').setAttribute('data-total', dataCoffee[i].price);
+                            totalCardPrice = dataCoffee[i].price;
                             
                             
                             // size coffee
@@ -189,7 +262,7 @@ const coffieMenuSettings = () => {
                             Object.keys(dataCoffee[i].additives).forEach((elem) => {
                                 advitivesCoffee += `
                                     <label class="popup-coffee-settings-item f-center" data-dopprice="${Object.values(dataCoffee[i].additives[elem])[1]}">
-                                        <input type="radio" name="size">
+                                        <input type="radio" name="additives">
                                         <span class="bg"></span>
                                         <span class="icon f-center-center">${Number(elem) + 1}</span>
                                         <span class="name">${Object.values(dataCoffee[i].additives[elem])[0]}</span>
@@ -199,12 +272,52 @@ const coffieMenuSettings = () => {
                             modalCoffeeElem.querySelector('.popup-coffee-block-additives .popup-coffee-settings').innerHTML = advitivesCoffee;
                         }
                     }
-                    modalCoffeeElem.classList.add('open')
+                    modalCoffeeElem.classList.add('open');
+                    document.querySelector('html').classList.add('hidden');
+                    selectCardSettings(totalCardPrice);
                 })
             })
         }
         openModal()
+
+        const closeModal = () => {
+            document.querySelector('.popup-coffee__close').addEventListener('click', () => {
+                document.querySelector('html').classList.remove('hidden');
+                modalCoffeeElem.classList.remove('open');
+            })
+            document.addEventListener('click', (e) => {
+                if(e.target.classList.contains('duty')) {
+                    document.querySelector('html').classList.remove('hidden');
+                    modalCoffeeElem.classList.remove('open');
+                }
+            })
+            document.addEventListener('keydown', (e) => {
+                if (e.key === "Escape") {
+                    document.querySelector('html').classList.remove('hidden');
+                    modalCoffeeElem.classList.remove('open');
+                }
+            })
+        }
+        closeModal()
+
+        const selectCardSettings = (totalCardPrice) => {
+            const checkboxModal = modalCoffeeElem.querySelectorAll('.popup-coffee-settings-item');
+            let dopPriceTotal = 0;
+            checkboxModal.forEach((e) => {
+                e.addEventListener('change', (e) => {
+                    dopPriceTotal = 0;
+                    checkboxModal.forEach((elem) => {
+                        if(elem.querySelector('input').checked) {
+                            dopPriceTotal += Number(elem.getAttribute('data-dopprice'))
+                        }
+                    })
+                    modalCoffeeElem.querySelector('.popup-coffee-total__price').textContent = '$' + (Number(totalCardPrice) + dopPriceTotal).toFixed(2);
+                })
+            })
+        }
         
     }
+    
+
 }
 coffieMenuSettings()
